edc822fc4dc54a0876d5d72ff170bfd29c48ee60
hungnv <hungnv@rabiloo.com.vn>
2021-05-12 14:57:56 +0700

    add mmkv storage hook

---
+++ src/useMMKVStorage.ts
@@ -0,0 +1,10 @@
+import MMKVStorage, {useMMKVStorage} from 'react-native-mmkv-storage'
+
+const MMKV: MMKVStorage.API = new MMKVStorage.Loader()
+  .withInstanceID('mmkv-app-key-hook').initialize()
+type LiteralUnion<T extends U, U = string> = T | (U & {})
+
+export const useStorage = (key: LiteralUnion<string>) => {
+  const [value, setValue] = useMMKVStorage(key, MMKV);
+  return [value, setValue];
+};
